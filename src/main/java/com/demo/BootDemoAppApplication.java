b432b4af57718945c7a87c3877903d5c235d3a1f
sagar241990 <sagar241990@gmail.com>
2023-09-15 12:04:28 +0530

    change

--- src/main/java/com/demo/BootDemoAppApplication.java
+++ src/main/java/com/demo/BootDemoAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootDemoAppApplication {
     public static void main(String[] args) {
+    	System.out.println("woeeeee");
         SpringApplication.run(BootDemoAppApplication.class, args);
     }
 
