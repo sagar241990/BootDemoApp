d84cec62f8bc657113146ad7539fee097d5af35a
sagar241990 <sagar241990@gmail.com>
2023-09-15 12:01:50 +0530

    new changes

---
+++ src/main/java/com/demo/controllers/PersonController.java
@@ -0,0 +1,22 @@
+package com.demo.controllers;
+
+
+import com.demo.entites.Person;
+import com.demo.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/persons")
+    public ResponseEntity<?> getAllPersons() {
+    	System.out.println("hello sagar");
+        return ResponseEntity.ok(this.personService.getAllPerson());
+    }
+}
